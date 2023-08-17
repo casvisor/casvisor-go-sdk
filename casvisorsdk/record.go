e8e96ff910f0a782402841429bec8821c577c3cf
liuqs <luiquansheng@qq.com>
2023-08-17 23:55:38 +0800

    feat: support delete-record/update-record.

--- casvisorsdk/record.go
+++ casvisorsdk/record.go
@@ -1,7 +1,22 @@
+// Copyright 2023 The Casvisor Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package casvisorsdk
 
 import (
 	"encoding/json"
+	"strconv"
 )
 
 type Record struct {
@@ -43,4 +58,59 @@ func (c *Client) AddRecord(record *Record) (bool, error) {
 	}
 
 	return resp.Data == "Affected", nil
+}
+
+func (c *Client) GetRecords() ([]*Record, error) {
+	queryMap := map[string]string{
+		"owner": c.OrganizationName,
+	}
+
+	url := c.GetUrl("get-records", queryMap)
+
+	bytes, err := c.DoGetBytes(url)
+	if err != nil {
+		return nil, err
+	}
+
+	var records []*Record
+	err = json.Unmarshal(bytes, &records)
+	if err != nil {
+		return nil, err
+	}
+	return records, nil
+}
+
+func (c *Client) GetPaginationRecords(p int, pageSize int, queryMap map[string]string) ([]*Record, int, error) {
+	queryMap["owner"] = c.OrganizationName
+	queryMap["p"] = strconv.Itoa(p)
+	queryMap["pageSize"] = strconv.Itoa(pageSize)
+
+	url := c.GetUrl("get-records", queryMap)
+
+	response, err := c.DoGetResponse(url)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	bytes, err := json.Marshal(response.Data)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	var records []*Record
+	err = json.Unmarshal(bytes, &records)
+	if err != nil {
+		return nil, 0, err
+	}
+	return records, int(response.Data2.(float64)), nil
+}
+
+func (c *Client) UpdateRecord(record *Record) (bool, error) {
+	_, affected, err := c.modifyRecord("update-record", record, nil)
+	return affected, err
+}
+
+func (c *Client) DeleteRecord(record *Record) (bool, error) {
+	_, affected, err := c.modifyRecord("delete-record", record, nil)
+	return affected, err
 }
